504d6b79b54a2ba951f6e322740df377a795740c
ph@phloc.com <ph@phloc.com@0ecd13d9-db3c-ce95-be93-8bf94d4a4a5e>
2012-06-15 13:02:50 +0000

    * Added possibility to provide a custom exception handler to handle the generated ParseExceptions * Small speed up in parsing

---
+++ trunk/src/main/java/com/phloc/css/handler/ICSSParseExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.phloc.css.handler;
+
+import com.phloc.commons.callback.IExceptionHandler;
+import com.phloc.css.parser.ParseException;
+
+public interface ICSSParseExceptionHandler extends IExceptionHandler <ParseException>
+{
+  /* empty */
+}
