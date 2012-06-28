1759dc3d7d92e02d21026a533567f3f58c90ea54
ph@phloc.com <ph@phloc.com@0ecd13d9-db3c-ce95-be93-8bf94d4a4a5e>
2012-06-28 19:03:51 +0000

    Added some more constants

--- trunk/src/main/java/com/phloc/css/propertyvalue/CCSSValue.java
+++ trunk/src/main/java/com/phloc/css/propertyvalue/CCSSValue.java
@@ -37,9 +37,13 @@ public final class CCSSValue
   public static final String PREFIX_URL = "url";
   public static final String PREFIX_URL_OPEN = PREFIX_URL + '(';
   public static final String PREFIX_RGB = "rgb";
+  public static final String PREFIX_RGB_OPEN = PREFIX_RGB + '(';
   public static final String PREFIX_RGBA = "rgba";
+  public static final String PREFIX_RGBA_OPEN = PREFIX_RGBA + '(';
   public static final String PREFIX_HSL = "hsl";
+  public static final String PREFIX_HSL_OPEN = PREFIX_HSL + '(';
   public static final String PREFIX_HSLA = "hsla";
+  public static final String PREFIX_HSLA_OPEN = PREFIX_HSLA + '(';
   public static final int HEXVALUE_LENGTH = 7;
   public static final char PREFIX_HEX = '#';
 
