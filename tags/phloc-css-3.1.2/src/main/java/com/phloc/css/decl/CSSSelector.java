78f611db17dd80380b427dce107652ca45b22026
ph@phloc.com <ph@phloc.com@0ecd13d9-db3c-ce95-be93-8bf94d4a4a5e>
2012-08-08 09:18:28 +0000

    [maven-release-plugin]  copy for tag phloc-css-3.1.2

---
+++ tags/phloc-css-3.1.2/src/main/java/com/phloc/css/decl/CSSSelector.java
@@ -0,0 +1,106 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.css.decl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+
+import com.phloc.commons.annotations.ReturnsMutableCopy;
+import com.phloc.commons.collections.ContainerHelper;
+import com.phloc.commons.hash.HashCodeGenerator;
+import com.phloc.commons.state.EChange;
+import com.phloc.commons.string.ToStringGenerator;
+import com.phloc.css.ICSSWriteable;
+import com.phloc.css.ICSSWriterSettings;
+
+/**
+ * Represents a single selector as the aggregation of selector members.
+ * 
+ * @author philip
+ */
+public final class CSSSelector implements ICSSWriteable
+{
+  private final List <ICSSSelectorMember> m_aMembers = new ArrayList <ICSSSelectorMember> ();
+
+  public CSSSelector ()
+  {}
+
+  public void addMember (@Nonnull final ICSSSelectorMember aMember)
+  {
+    if (aMember == null)
+      throw new NullPointerException ("member");
+    m_aMembers.add (aMember);
+  }
+
+  @Nonnull
+  public EChange removeMember (@Nonnull final ICSSSelectorMember aMember)
+  {
+    return EChange.valueOf (m_aMembers.remove (aMember));
+  }
+
+  @Nonnull
+  public EChange removeMember (@Nonnegative final int nMemberIndex)
+  {
+    if (nMemberIndex < 0 || nMemberIndex >= m_aMembers.size ())
+      return EChange.UNCHANGED;
+    m_aMembers.remove (nMemberIndex);
+    return EChange.CHANGED;
+  }
+
+  @Nonnull
+  @ReturnsMutableCopy
+  public List <ICSSSelectorMember> getAllMembers ()
+  {
+    return ContainerHelper.newList (m_aMembers);
+  }
+
+  @Nonnull
+  public String getAsCSSString (@Nonnull final ICSSWriterSettings aSettings, @Nonnegative final int nIndentLevel)
+  {
+    final StringBuilder aSB = new StringBuilder ();
+    for (final ICSSSelectorMember aMember : m_aMembers)
+      aSB.append (aMember.getAsCSSString (aSettings, nIndentLevel));
+    return aSB.toString ();
+  }
+
+  @Override
+  public boolean equals (final Object o)
+  {
+    if (o == this)
+      return true;
+    if (!(o instanceof CSSSelector))
+      return false;
+    final CSSSelector rhs = (CSSSelector) o;
+    return m_aMembers.equals (rhs.m_aMembers);
+  }
+
+  @Override
+  public int hashCode ()
+  {
+    return new HashCodeGenerator (this).append (m_aMembers).getHashCode ();
+  }
+
+  @Override
+  public String toString ()
+  {
+    return new ToStringGenerator (this).append ("members", m_aMembers).toString ();
+  }
+}
---
+++ tags/phloc-css-3.1.2/src/main/java/com/phloc/css/decl/CSSURI.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.css.decl;
+
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.NotThreadSafe;
+
+import com.phloc.commons.annotations.Nonempty;
+import com.phloc.commons.hash.HashCodeGenerator;
+import com.phloc.commons.string.StringHelper;
+import com.phloc.commons.string.ToStringGenerator;
+import com.phloc.css.ICSSWriteable;
+import com.phloc.css.ICSSWriterSettings;
+import com.phloc.css.utils.CSSURLHelper;
+
+/**
+ * Represents a single CSS URI
+ * 
+ * @author philip
+ */
+@NotThreadSafe
+public final class CSSURI implements ICSSWriteable
+{
+  private String m_sURI;
+
+  public CSSURI (@Nonnull @Nonempty final String sURI)
+  {
+    setURI (sURI);
+  }
+
+  /**
+   * @return The URL
+   */
+  @Nonnull
+  @Nonempty
+  public String getURI ()
+  {
+    return m_sURI;
+  }
+
+  public void setURI (@Nonnull @Nonempty final String sURI)
+  {
+    if (StringHelper.hasNoText (sURI))
+      throw new IllegalArgumentException ("URI may not be empty");
+    if (CSSURLHelper.isURLValue (sURI))
+      throw new IllegalArgumentException ("Only the URI and not the CSS URI value must be passed!");
+    m_sURI = sURI;
+  }
+
+  @Nonnull
+  @Nonempty
+  public String getAsCSSString (@Nonnull final ICSSWriterSettings aSettings, @Nonnegative final int nIndentLevel)
+  {
+    return CSSURLHelper.getAsCSSURL (m_sURI, aSettings.isQuoteURLs ());
+  }
+
+  @Override
+  public boolean equals (final Object o)
+  {
+    if (o == this)
+      return true;
+    if (!(o instanceof CSSURI))
+      return false;
+    final CSSURI rhs = (CSSURI) o;
+    return m_sURI.equals (rhs.m_sURI);
+  }
+
+  @Override
+  public int hashCode ()
+  {
+    return new HashCodeGenerator (this).append (m_sURI).getHashCode ();
+  }
+
+  @Override
+  public String toString ()
+  {
+    return new ToStringGenerator (this).append ("URI", m_sURI).toString ();
+  }
+}
