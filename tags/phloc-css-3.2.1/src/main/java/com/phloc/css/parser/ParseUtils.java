872741585eaabe903743cda787c23844cfb8b952
ph@phloc.com <ph@phloc.com@0ecd13d9-db3c-ce95-be93-8bf94d4a4a5e>
2012-09-22 09:59:47 +0000

    [maven-release-plugin]  copy for tag phloc-css-3.2.1

---
+++ tags/phloc-css-3.2.1/src/main/java/com/phloc/css/parser/ParseUtils.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.css.parser;
+
+import java.util.regex.Matcher;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.Immutable;
+
+import com.phloc.commons.regex.RegExHelper;
+import com.phloc.commons.string.StringHelper;
+
+/**
+ * This class is used by the generated parsers to do some common stuff.
+ * 
+ * @author philip
+ */
+@Immutable
+public final class ParseUtils
+{
+  private ParseUtils ()
+  {}
+
+  @Nonnull
+  private static String _trimBy (@Nonnull final CharSequence s, final int nLeftSkip, final int nRightSkip)
+  {
+    return s.toString ().substring (nLeftSkip, s.length () - nRightSkip);
+  }
+
+  /**
+   * Remove surrounding quotes (single or double) of a string (if present). If
+   * the start and the end quote are not equal, nothing happens.
+   * 
+   * @param sStr
+   *        The string where the quotes should be removed
+   * @return The string without quotes.
+   */
+  @Nullable
+  public static String extractStringValue (@Nullable final String sStr)
+  {
+    if (StringHelper.hasNoText (sStr) || sStr.length () < 2)
+      return sStr;
+
+    final char cFirst = sStr.charAt (0);
+    if ((cFirst == '"' || cFirst == '\'') && StringHelper.getLastChar (sStr) == cFirst)
+    {
+      // Remove quotes around the string
+      return _trimBy (sStr, 1, 1);
+    }
+    return sStr;
+  }
+
+  /**
+   * Remove the leading "url(" and the trailing ")" from an URL CSS value. No
+   * check is performed for the existence of a leading "url("!
+   * 
+   * @param s
+   *        The value to remove the string from.
+   * @return The trimmed value.
+   */
+  @Nonnull
+  public static String trimUrl (@Nonnull final CharSequence s)
+  {
+    // Extract from "url(...)"
+    final String s1 = _trimBy (s, 4, 1).trim ();
+    return extractStringValue (s1);
+  }
+
+  @Nonnull
+  public static String splitNumber (@Nonnull final StringBuilder aPattern)
+  {
+    // Find the longest matching number within the pattern
+    // Order of the rules in brackets is important!
+    final String sRegEx = "^([0-9]*\\.[0-9]+|[0-9]+).*$";
+    final Matcher m = RegExHelper.getMatcher (sRegEx, aPattern.toString ());
+    if (m.matches ())
+      return m.group (1);
+    return "";
+  }
+}
