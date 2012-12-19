449e8b56de0eb8f6c7118876117c52d777d4c2f5
ph <ph@phloc.com>
2012-12-19 17:51:21 +0000

    Added comments and added method to add rules at a specified index

--- src/main/java/com/phloc/css/decl/CascadingStyleSheet.java
+++ src/main/java/com/phloc/css/decl/CascadingStyleSheet.java
@@ -49,17 +49,30 @@ public final class CascadingStyleSheet
   public CascadingStyleSheet ()
   {}
 
+  /**
+   * @return <code>true</code> if at least one <code>@import</code> rule is
+   *         present, <code>false</code> otherwise.
+   */
   public boolean hasImportRules ()
   {
     return !m_aImportRules.isEmpty ();
   }
 
+  /**
+   * @return The number of contained <code>@import</code> rules. Always &ge; 0.
+   */
   @Nonnegative
   public int getImportRuleCount ()
   {
     return m_aImportRules.size ();
   }
 
+  /**
+   * Add a new <code>@import</code> rule at the end.
+   * 
+   * @param aImportRule
+   *        The import rule to add. May not be <code>null</code>.
+   */
   public void addImportRule (@Nonnull final CSSImportRule aImportRule)
   {
     if (aImportRule == null)
@@ -67,12 +80,43 @@ public final class CascadingStyleSheet
     m_aImportRules.add (aImportRule);
   }
 
+  /**
+   * Add a new <code>@import</code> rule at a specified index.
+   * 
+   * @param nIndex
+   *        The index where the rule should be added.
+   * @param aImportRule
+   *        The import rule to add. May not be <code>null</code>.
+   * @throws ArrayIndexOutOfBoundsException
+   *         if the index is invalid
+   */
+  public void addImportRule (@Nonnegative final int nIndex, @Nonnull final CSSImportRule aImportRule)
+  {
+    if (aImportRule == null)
+      throw new NullPointerException ("ImportRule");
+    m_aImportRules.add (nIndex, aImportRule);
+  }
+
+  /**
+   * Remove the specified <code>@import</code> rule.
+   * 
+   * @param aImportRule
+   *        The import rule to be removed. May be <code>null</code>.
+   * @return {@link EChange#CHANGED} if removal was successful.
+   */
   @Nonnull
   public EChange removeImportRule (@Nullable final CSSImportRule aImportRule)
   {
     return EChange.valueOf (m_aImportRules.remove (aImportRule));
   }
 
+  /**
+   * Remove the <code>@import</code> rule at the specified index.
+   * 
+   * @param nImportRuleIndex
+   *        The index to be removed.
+   * @return {@link EChange#CHANGED} if removal was successful.
+   */
   @Nonnull
   public EChange removeImportRule (@Nonnegative final int nImportRuleIndex)
   {
@@ -82,6 +126,10 @@ public final class CascadingStyleSheet
     return EChange.CHANGED;
   }
 
+  /**
+   * @return A copy of all contained <code>@import</code> rules. Never
+   *         <code>null</code>.
+   */
   @Nonnull
   @ReturnsMutableCopy
   public List <CSSImportRule> getAllImportRules ()
@@ -89,17 +137,31 @@ public final class CascadingStyleSheet
     return ContainerHelper.newList (m_aImportRules);
   }
 
+  /**
+   * @return <code>true</code> if at least one <code>@namespace</code> rule is
+   *         present, <code>false</code> otherwise.
+   */
   public boolean hasNamespaceRules ()
   {
     return !m_aNamespaceRules.isEmpty ();
   }
 
+  /**
+   * @return The number of contained <code>@namespace</code> rules. Always &ge;
+   *         0.
+   */
   @Nonnegative
   public int getNamespaceRuleCount ()
   {
     return m_aNamespaceRules.size ();
   }
 
+  /**
+   * Add a new <code>@namespace</code> rule at the end.
+   * 
+   * @param aNamespaceRule
+   *        The namespace rule to be added. May not be <code>null</code>.
+   */
   public void addNamespaceRule (@Nonnull final CSSNamespaceRule aNamespaceRule)
   {
     if (aNamespaceRule == null)
@@ -107,12 +169,43 @@ public final class CascadingStyleSheet
     m_aNamespaceRules.add (aNamespaceRule);
   }
 
+  /**
+   * Add a new <code>@namespace</code> rule at the specified index.
+   * 
+   * @param nIndex
+   *        The index where the rule should be added.
+   * @param aNamespaceRule
+   *        The namespace rule to be added. May not be <code>null</code>.
+   */
+  public void addNamespaceRule (@Nonnegative final int nIndex, @Nonnull final CSSNamespaceRule aNamespaceRule)
+  {
+    if (aNamespaceRule == null)
+      throw new NullPointerException ("NamespaceRule");
+    m_aNamespaceRules.add (nIndex, aNamespaceRule);
+  }
+
+  /**
+   * Remove the specified <code>@namespace</code> rule.
+   * 
+   * @param aNamespaceRule
+   *        The namespace rule to be removed. May be <code>null</code>.
+   * @return {@link EChange#CHANGED} if the namespace rule was successfully
+   *         removed
+   */
   @Nonnull
   public EChange removeNamespaceRule (@Nullable final CSSNamespaceRule aNamespaceRule)
   {
     return EChange.valueOf (m_aNamespaceRules.remove (aNamespaceRule));
   }
 
+  /**
+   * Remove the <code>@namespace</code> rule at the specified index.
+   * 
+   * @param nNamespaceRuleIndex
+   *        The index to be removed.
+   * @return {@link EChange#CHANGED} if the namespace rule was successfully
+   *         removed
+   */
   @Nonnull
   public EChange removeNamespaceRule (@Nonnegative final int nNamespaceRuleIndex)
   {
@@ -122,6 +215,10 @@ public final class CascadingStyleSheet
     return EChange.CHANGED;
   }
 
+  /**
+   * @return A copy of all <code>@namespace</code> rules. Never
+   *         <code>null</code>.
+   */
   @Nonnull
   @ReturnsMutableCopy
   public List <CSSNamespaceRule> getAllNamespaceRules ()
@@ -129,30 +226,77 @@ public final class CascadingStyleSheet
     return ContainerHelper.newList (m_aNamespaceRules);
   }
 
+  /**
+   * Check if any top-level rule is present. This method only considers
+   * top-level rules and not import and namespace rules!
+   * 
+   * @return <code>true</code> if at least one top-level rule is present,
+   *         <code>false</code> if otherwise.
+   */
   public boolean hasRules ()
   {
     return !m_aRules.isEmpty ();
   }
 
+  /**
+   * @return The number of total contained top-level rules. Always &ge; 0.
+   */
   @Nonnegative
   public int getRuleCount ()
   {
     return m_aRules.size ();
   }
 
-  public void addRule (@Nonnull final ICSSTopLevelRule aStyleRule)
+  /**
+   * Add a new top-level rule at the end.
+   * 
+   * @param aRule
+   *        The rule to be added. May not be <code>null</code>.
+   */
+  public void addRule (@Nonnull final ICSSTopLevelRule aRule)
   {
-    if (aStyleRule == null)
+    if (aRule == null)
       throw new NullPointerException ("styleRule");
-    m_aRules.add (aStyleRule);
+    m_aRules.add (aRule);
   }
 
+  /**
+   * Add a new top-level rule at the specified index.
+   * 
+   * @param nIndex
+   *        The index where the top-level rule should be added. Must be &ge; 0.
+   * @param aRule
+   *        The rule to be added. May not be <code>null</code>.
+   */
+  public void addRule (@Nonnegative final int nIndex, @Nonnull final ICSSTopLevelRule aRule)
+  {
+    if (aRule == null)
+      throw new NullPointerException ("styleRule");
+    m_aRules.add (nIndex, aRule);
+  }
+
+  /**
+   * Remove the specified top-level rule.
+   * 
+   * @param aRule
+   *        The rule to be removed. May be <code>null</code>.
+   * @return {@link EChange#CHANGED} if the rule was successfully removed.
+   */
   @Nonnull
   public EChange removeRule (@Nullable final ICSSTopLevelRule aRule)
   {
     return EChange.valueOf (m_aRules.remove (aRule));
   }
 
+  /**
+   * Remove the rule at the specified index.
+   * 
+   * @param nRuleIndex
+   *        The index of the rule to be removed.
+   * @return {@link EChange#CHANGED} if the rule at the specified index was
+   *         successfully removed
+   */
+  @Nonnull
   public EChange removeRule (@Nonnegative final int nRuleIndex)
   {
     if (nRuleIndex < 0 || nRuleIndex >= m_aRules.size ())
@@ -161,6 +305,9 @@ public final class CascadingStyleSheet
     return EChange.CHANGED;
   }
 
+  /**
+   * @return A copy of all contained top-level rules. Never <code>null</code>.
+   */
   @Nonnull
   @ReturnsMutableCopy
   public List <ICSSTopLevelRule> getAllRules ()
@@ -168,6 +315,10 @@ public final class CascadingStyleSheet
     return ContainerHelper.newList (m_aRules);
   }
 
+  /**
+   * @return <code>true</code> if at least one style rule is contained,
+   *         <code>false</code> otherwise.
+   */
   public boolean hasStyleRules ()
   {
     for (final ICSSTopLevelRule aTopLevelRule : m_aRules)
@@ -176,6 +327,9 @@ public final class CascadingStyleSheet
     return false;
   }
 
+  /**
+   * @return The number of contained style rules. Always &ge; 0.
+   */
   @Nonnegative
   public int getStyleRuleCount ()
   {
@@ -186,6 +340,9 @@ public final class CascadingStyleSheet
     return ret;
   }
 
+  /**
+   * @return A copy of all contained style rules. Never <code>null</code>.
+   */
   @Nonnull
   @ReturnsMutableCopy
   public List <CSSStyleRule> getAllStyleRules ()
@@ -197,6 +354,10 @@ public final class CascadingStyleSheet
     return ret;
   }
 
+  /**
+   * @return <code>true</code> if at least one <code>@page</code> rule is
+   *         contained, <code>false</code> otherwise.
+   */
   public boolean hasPageRules ()
   {
     for (final ICSSTopLevelRule aTopLevelRule : m_aRules)
@@ -205,6 +366,9 @@ public final class CascadingStyleSheet
     return false;
   }
 
+  /**
+   * @return The number of contained <code>@page</code> rules. Always &ge; 0.
+   */
   @Nonnegative
   public int getPageRuleCount ()
   {
@@ -215,6 +379,10 @@ public final class CascadingStyleSheet
     return ret;
   }
 
+  /**
+   * @return A copy of all contained <code>@page</code> rules. Never
+   *         <code>null</code>.
+   */
   @Nonnull
   @ReturnsMutableCopy
   public List <CSSPageRule> getAllPageRules ()
@@ -226,6 +394,10 @@ public final class CascadingStyleSheet
     return ret;
   }
 
+  /**
+   * @return <code>true</code> if at least one <code>@media</code> rule is
+   *         contained, <code>false</code> otherwise.
+   */
   public boolean hasMediaRules ()
   {
     for (final ICSSTopLevelRule aTopLevelRule : m_aRules)
@@ -234,6 +406,9 @@ public final class CascadingStyleSheet
     return false;
   }
 
+  /**
+   * @return The number of contained <code>@media</code> rules. Always &ge; 0.
+   */
   @Nonnegative
   public int getMediaRuleCount ()
   {
@@ -244,6 +419,10 @@ public final class CascadingStyleSheet
     return ret;
   }
 
+  /**
+   * @return A copy of all contained <code>@media</code> rules. Never
+   *         <code>null</code>.
+   */
   @Nonnull
   @ReturnsMutableCopy
   public List <CSSMediaRule> getAllMediaRules ()
@@ -255,6 +434,10 @@ public final class CascadingStyleSheet
     return ret;
   }
 
+  /**
+   * @return <code>true</code> if at least one <code>@font-face</code> rule is
+   *         contained, <code>false</code> otherwise.
+   */
   public boolean hasFontFaceRules ()
   {
     for (final ICSSTopLevelRule aTopLevelRule : m_aRules)
@@ -263,6 +446,10 @@ public final class CascadingStyleSheet
     return false;
   }
 
+  /**
+   * @return The number of contained <code>@font-face</code> rules. Always &ge;
+   *         0.
+   */
   @Nonnegative
   public int getFontFaceRuleCount ()
   {
@@ -273,6 +460,10 @@ public final class CascadingStyleSheet
     return ret;
   }
 
+  /**
+   * @return A copy of all contained <code>@font-face</code> rules. Never
+   *         <code>null</code>.
+   */
   @Nonnull
   @ReturnsMutableCopy
   public List <CSSFontFaceRule> getAllFontFaceRules ()
@@ -284,6 +475,10 @@ public final class CascadingStyleSheet
     return ret;
   }
 
+  /**
+   * @return <code>true</code> if at least one <code>@keyframes</code> rule is
+   *         contained, <code>false</code> otherwise.
+   */
   public boolean hasKeyframesRules ()
   {
     for (final ICSSTopLevelRule aTopLevelRule : m_aRules)
@@ -292,6 +487,10 @@ public final class CascadingStyleSheet
     return false;
   }
 
+  /**
+   * @return The number of contained <code>@keyframes</code> rules. Always &ge;
+   *         0.
+   */
   @Nonnegative
   public int getKeyframesRuleCount ()
   {
@@ -302,6 +501,10 @@ public final class CascadingStyleSheet
     return ret;
   }
 
+  /**
+   * @return A copy of all contained <code>@keyframes</code> rules. Never
+   *         <code>null</code>.
+   */
   @Nonnull
   @ReturnsMutableCopy
   public List <CSSKeyframesRule> getAllKeyframesRules ()
@@ -313,6 +516,10 @@ public final class CascadingStyleSheet
     return ret;
   }
 
+  /**
+   * @return <code>true</code> if at least one <code>@viewport</code> rule is
+   *         contained, <code>false</code> otherwise.
+   */
   public boolean hasViewportRules ()
   {
     for (final ICSSTopLevelRule aTopLevelRule : m_aRules)
@@ -321,6 +528,10 @@ public final class CascadingStyleSheet
     return false;
   }
 
+  /**
+   * @return The number of contained <code>@viewport</code> rules. Always &ge;
+   *         0.
+   */
   @Nonnegative
   public int getViewportRuleCount ()
   {
@@ -331,6 +542,10 @@ public final class CascadingStyleSheet
     return ret;
   }
 
+  /**
+   * @return A copy of all contained <code>@viewport</code> rules. Never
+   *         <code>null</code>.
+   */
   @Nonnull
   @ReturnsMutableCopy
   public List <CSSViewportRule> getAllViewportRules ()
