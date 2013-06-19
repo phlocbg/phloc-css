72ed2e61f7a7ee03b4fd5a0a258c4cee3c46406a
ph@phloc.com <ph@phloc.com@0ecd13d9-db3c-ce95-be93-8bf94d4a4a5e>
2013-06-19 05:05:42 +0000

    [maven-release-plugin]  copy for tag phloc-css-jdk5-3.5.4

---
+++ tags/phloc-css-jdk5-3.5.4/src/main/java/com/phloc/css/property/CSSPropertyFree.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (C) 2006-2013 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.css.property;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.OverridingMethodsMustInvokeSuper;
+
+import com.phloc.commons.string.StringHelper;
+
+/**
+ * CSS property without any layout constraint, except that it may not be empty.
+ * 
+ * @author Philip Helger
+ */
+public class CSSPropertyFree extends AbstractCSSProperty
+{
+  public CSSPropertyFree (@Nonnull final ECSSProperty eProp)
+  {
+    super (eProp);
+  }
+
+  @Override
+  @OverridingMethodsMustInvokeSuper
+  public boolean isValidValue (@Nullable final String sValue)
+  {
+    return super.isValidValue (sValue) || StringHelper.hasText (sValue);
+  }
+
+  @Nonnull
+  public CSSPropertyFree getClone (@Nonnull final ECSSProperty eProp)
+  {
+    return new CSSPropertyFree (eProp);
+  }
+}
---
+++ tags/phloc-css-jdk5-3.5.4/src/test/java/com/phloc/css/reader/CSSReader21Test.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (C) 2006-2013 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.css.reader;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.phloc.commons.charset.CCharset;
+import com.phloc.css.ECSSVersion;
+
+/**
+ * Test reading CSS 2.1 files
+ * 
+ * @author Philip Helger
+ */
+public final class CSSReader21Test extends AbstractFuncTestCSSReader
+{
+  public CSSReader21Test ()
+  {
+    super (ECSSVersion.CSS21, CCharset.CHARSET_UTF_8_OBJ, false);
+  }
+
+  @Test
+  public void testReadAll21Good () throws IOException
+  {
+    testReadGood ("src/test/resources/testfiles/css21/good");
+    testReadGood ("src/test/resources/testfiles/css21/bad_but_succeeding");
+  }
+
+  @Test
+  public void testReadAll21Bad () throws IOException
+  {
+    testReadBad ("src/test/resources/testfiles/css21/bad");
+    testReadBad ("src/test/resources/testfiles/css21/good_but_failing");
+  }
+
+  @Test
+  public void testReadAll21BadButRecoverable () throws IOException
+  {
+    testReadBadButRecoverable ("src/test/resources/testfiles/css21/bad_but_recoverable");
+  }
+}
