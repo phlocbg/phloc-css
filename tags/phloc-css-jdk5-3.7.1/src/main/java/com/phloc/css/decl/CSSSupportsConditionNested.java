974bf98a82b05fb5a7bec78508c0cc878a88eee1
ph@phloc.com <ph@phloc.com@0ecd13d9-db3c-ce95-be93-8bf94d4a4a5e>
2014-01-29 12:05:46 +0000

    [maven-release-plugin]  copy for tag phloc-css-jdk5-3.7.1

---
+++ tags/phloc-css-jdk5-3.7.1/src/main/java/com/phloc/css/decl/CSSSupportsConditionNested.java
@@ -0,0 +1,225 @@
+/**
+ * Copyright (C) 2006-2014 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.css.decl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.NotThreadSafe;
+
+import com.phloc.commons.annotations.Nonempty;
+import com.phloc.commons.annotations.ReturnsMutableCopy;
+import com.phloc.commons.collections.ContainerHelper;
+import com.phloc.commons.hash.HashCodeGenerator;
+import com.phloc.commons.state.EChange;
+import com.phloc.commons.string.ToStringGenerator;
+import com.phloc.css.CSSSourceLocation;
+import com.phloc.css.ECSSVersion;
+import com.phloc.css.ICSSSourceLocationAware;
+import com.phloc.css.ICSSWriterSettings;
+
+/**
+ * Represents a single negation supports condition
+ * 
+ * @author Philip Helger
+ */
+@NotThreadSafe
+public class CSSSupportsConditionNested implements ICSSSupportsConditionMember, ICSSSourceLocationAware
+{
+  private final List <ICSSSupportsConditionMember> m_aMembers = new ArrayList <ICSSSupportsConditionMember> ();
+  private CSSSourceLocation m_aSourceLocation;
+
+  public CSSSupportsConditionNested ()
+  {}
+
+  public boolean hasMembers ()
+  {
+    return !m_aMembers.isEmpty ();
+  }
+
+  /**
+   * @deprecated Use {@link #getMemberCount()} instead
+   */
+  @Deprecated
+  @Nonnegative
+  public int getSupportsMemberCount ()
+  {
+    return getMemberCount ();
+  }
+
+  @Nonnegative
+  public int getMemberCount ()
+  {
+    return m_aMembers.size ();
+  }
+
+  @Nonnull
+  public CSSSupportsConditionNested addMember (@Nonnull final ICSSSupportsConditionMember aMember)
+  {
+    if (aMember == null)
+      throw new NullPointerException ("member");
+
+    m_aMembers.add (aMember);
+    return this;
+  }
+
+  @Nonnull
+  public CSSSupportsConditionNested addMember (@Nonnegative final int nIndex,
+                                               @Nonnull final ICSSSupportsConditionMember aMember)
+  {
+    if (nIndex < 0)
+      throw new IllegalArgumentException ("Index too small: " + nIndex);
+    if (aMember == null)
+      throw new NullPointerException ("member");
+
+    if (nIndex >= getMemberCount ())
+      m_aMembers.add (aMember);
+    else
+      m_aMembers.add (nIndex, aMember);
+    return this;
+  }
+
+  /**
+   * @deprecated Use {@link #removeMember(ICSSSupportsConditionMember)} instead
+   */
+  @Deprecated
+  @Nonnull
+  public EChange removeSupportsMember (@Nonnull final ICSSSupportsConditionMember aMember)
+  {
+    return removeMember (aMember);
+  }
+
+  @Nonnull
+  public EChange removeMember (@Nonnull final ICSSSupportsConditionMember aMember)
+  {
+    return EChange.valueOf (m_aMembers.remove (aMember));
+  }
+
+  /**
+   * @deprecated Use {@link #removeMember(int)} instead
+   */
+  @Deprecated
+  @Nonnull
+  public EChange removeSupportsMember (@Nonnegative final int nIndex)
+  {
+    return removeMember (nIndex);
+  }
+
+  @Nonnull
+  public EChange removeMember (@Nonnegative final int nIndex)
+  {
+    if (nIndex < 0 || nIndex >= m_aMembers.size ())
+      return EChange.UNCHANGED;
+    m_aMembers.remove (nIndex);
+    return EChange.CHANGED;
+  }
+
+  /**
+   * @deprecated Use {@link #getMemberAtIndex(int)} instead
+   */
+  @Deprecated
+  @Nullable
+  public ICSSSupportsConditionMember getSupportsMemberAtIndex (@Nonnegative final int nIndex)
+  {
+    return getMemberAtIndex (nIndex);
+  }
+
+  @Nullable
+  public ICSSSupportsConditionMember getMemberAtIndex (@Nonnegative final int nIndex)
+  {
+    if (nIndex < 0 || nIndex >= m_aMembers.size ())
+      return null;
+    return m_aMembers.get (nIndex);
+  }
+
+  @Nonnull
+  @ReturnsMutableCopy
+  public List <ICSSSupportsConditionMember> getAllMembers ()
+  {
+    return ContainerHelper.newList (m_aMembers);
+  }
+
+  @Nonnull
+  @Nonempty
+  public String getAsCSSString (@Nonnull final ICSSWriterSettings aSettings, @Nonnegative final int nIndentLevel)
+  {
+    aSettings.checkVersionRequirements (this);
+    final StringBuilder aSB = new StringBuilder ("(");
+    boolean bFirst = true;
+    for (final ICSSSupportsConditionMember aMember : m_aMembers)
+    {
+      if (bFirst)
+        bFirst = false;
+      else
+        aSB.append (' ');
+      aSB.append (aMember.getAsCSSString (aSettings, nIndentLevel));
+    }
+    return aSB.append (')').toString ();
+  }
+
+  @Nonnull
+  public ECSSVersion getMinimumCSSVersion ()
+  {
+    return ECSSVersion.CSS30;
+  }
+
+  /**
+   * Set the source location of the object, determined while parsing.
+   * 
+   * @param aSourceLocation
+   *        The source location to use. May be <code>null</code>.
+   */
+  public void setSourceLocation (@Nullable final CSSSourceLocation aSourceLocation)
+  {
+    m_aSourceLocation = aSourceLocation;
+  }
+
+  @Nullable
+  public CSSSourceLocation getSourceLocation ()
+  {
+    return m_aSourceLocation;
+  }
+
+  @Override
+  public boolean equals (final Object o)
+  {
+    if (o == this)
+      return true;
+    if (!(o instanceof CSSSupportsConditionNested))
+      return false;
+    final CSSSupportsConditionNested rhs = (CSSSupportsConditionNested) o;
+    return m_aMembers.equals (rhs.m_aMembers);
+  }
+
+  @Override
+  public int hashCode ()
+  {
+    return new HashCodeGenerator (this).append (m_aMembers).getHashCode ();
+  }
+
+  @Override
+  public String toString ()
+  {
+    return new ToStringGenerator (this).append ("members", m_aMembers)
+                                       .appendIfNotNull ("sourceLocation", m_aSourceLocation)
+                                       .toString ();
+  }
+}
---
+++ tags/phloc-css-jdk5-3.7.1/src/main/java/com/phloc/css/reader/errorhandler/ICSSParseErrorHandler.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (C) 2006-2014 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.css.reader.errorhandler;
+
+import javax.annotation.Nonnull;
+
+import com.phloc.css.parser.ParseException;
+import com.phloc.css.parser.Token;
+
+/**
+ * Special CSS handler that is invoked during reading in case of a recoverable
+ * error.
+ * 
+ * @author Philip Helger
+ */
+public interface ICSSParseErrorHandler
+{
+  /**
+   * Called upon a recoverable error. The parameter list is similar to the one
+   * of the {@link com.phloc.css.parser.ParseException}.
+   * 
+   * @param aLastValidToken
+   *        The last valid token.
+   * @param aExpectedTokenSequencesVal
+   *        The expected token.
+   * @param aTokenImageVal
+   *        The error token image.
+   * @param aLastSkippedToken
+   *        The token until which was skipped (incl.)
+   * @throws ParseException
+   *         In case the error is fatal and should be propagated.
+   */
+  void onCSSParseError (@Nonnull Token aLastValidToken,
+                        @Nonnull int [][] aExpectedTokenSequencesVal,
+                        @Nonnull String [] aTokenImageVal,
+                        @Nonnull Token aLastSkippedToken) throws ParseException;
+}
