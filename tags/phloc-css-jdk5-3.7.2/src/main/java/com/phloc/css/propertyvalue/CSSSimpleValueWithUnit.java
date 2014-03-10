a8dba99faeb5928b21399ce83e9783f1616d8067
ph@phloc.com <ph@phloc.com@0ecd13d9-db3c-ce95-be93-8bf94d4a4a5e>
2014-03-10 08:57:21 +0000

    [maven-release-plugin]  copy for tag phloc-css-jdk5-3.7.2

---
+++ tags/phloc-css-jdk5-3.7.2/src/main/java/com/phloc/css/propertyvalue/CSSSimpleValueWithUnit.java
@@ -0,0 +1,171 @@
+/**
+ * Copyright (C) 2006-2014 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.css.propertyvalue;
+
+import java.io.Serializable;
+
+import javax.annotation.CheckReturnValue;
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.Immutable;
+
+import com.phloc.commons.equals.EqualsUtils;
+import com.phloc.commons.hash.HashCodeGenerator;
+import com.phloc.commons.string.ToStringGenerator;
+import com.phloc.css.ECSSUnit;
+
+/**
+ * This class encapsulates a single numeric value and a unit.
+ * 
+ * @author Philip Helger
+ */
+@Immutable
+public class CSSSimpleValueWithUnit implements Serializable
+{
+  private final double m_dValue;
+  private final ECSSUnit m_eUnit;
+
+  /**
+   * Constructor
+   * 
+   * @param dValue
+   *        Numeric value
+   * @param eUnit
+   *        CSS unit to use. May not be <code>null</code>.
+   */
+  public CSSSimpleValueWithUnit (final double dValue, @Nonnull final ECSSUnit eUnit)
+  {
+    if (eUnit == null)
+      throw new NullPointerException ("unit");
+    m_dValue = dValue;
+    m_eUnit = eUnit;
+  }
+
+  /**
+   * @return The numeric value as a decimal value
+   */
+  public double getValue ()
+  {
+    return m_dValue;
+  }
+
+  /**
+   * @return The numeric value as an int value - no check for validity is
+   *         performed
+   */
+  public int getAsIntValue ()
+  {
+    return (int) m_dValue;
+  }
+
+  /**
+   * @return The numeric value as a long value - no check for validity is
+   *         performed
+   */
+  public long getAsLongValue ()
+  {
+    return (long) m_dValue;
+  }
+
+  /**
+   * @return The CSS unit of this value. Never <code>null</code>.
+   */
+  @Nonnull
+  public ECSSUnit getUnit ()
+  {
+    return m_eUnit;
+  }
+
+  /**
+   * Get a new object with the same unit but an added value.
+   * 
+   * @param dDelta
+   *        The delta to be added.
+   * @return A new object. Never <code>null</code>.
+   */
+  @Nonnull
+  @CheckReturnValue
+  public CSSSimpleValueWithUnit add (final double dDelta)
+  {
+    return new CSSSimpleValueWithUnit (m_dValue + dDelta, m_eUnit);
+  }
+
+  /**
+   * Get a new object with the same unit but a subtracted value.
+   * 
+   * @param dDelta
+   *        The delta to be subtracted.
+   * @return A new object. Never <code>null</code>.
+   */
+  @Nonnull
+  @CheckReturnValue
+  public CSSSimpleValueWithUnit substract (final double dDelta)
+  {
+    return new CSSSimpleValueWithUnit (m_dValue - dDelta, m_eUnit);
+  }
+
+  /**
+   * Get a new object with the same unit but a multiplied value.
+   * 
+   * @param dValue
+   *        The value to be multiply with this value.
+   * @return A new object. Never <code>null</code>.
+   */
+  @Nonnull
+  @CheckReturnValue
+  public CSSSimpleValueWithUnit multiply (final double dValue)
+  {
+    return new CSSSimpleValueWithUnit (m_dValue * dValue, m_eUnit);
+  }
+
+  /**
+   * Get a new object with the same unit but an divided value.
+   * 
+   * @param dDivisor
+   *        The divisor to use.
+   * @return A new object. Never <code>null</code>.
+   */
+  @Nonnull
+  @CheckReturnValue
+  public CSSSimpleValueWithUnit divide (final double dDivisor)
+  {
+    return new CSSSimpleValueWithUnit (m_dValue / dDivisor, m_eUnit);
+  }
+
+  @Override
+  public boolean equals (final Object o)
+  {
+    if (o == this)
+      return true;
+    if (!(o instanceof CSSSimpleValueWithUnit))
+      return false;
+    final CSSSimpleValueWithUnit rhs = (CSSSimpleValueWithUnit) o;
+    return EqualsUtils.equals (m_dValue, rhs.m_dValue) && m_eUnit.equals (rhs.m_eUnit);
+  }
+
+  @Override
+  public int hashCode ()
+  {
+    return new HashCodeGenerator (this).append (m_dValue).append (m_eUnit).getHashCode ();
+  }
+
+  @Override
+  public String toString ()
+  {
+    return new ToStringGenerator (this).append ("value", m_dValue).append ("unit", m_eUnit).toString ();
+  }
+}
---
+++ tags/phloc-css-jdk5-3.7.2/src/main/java/com/phloc/css/reader/errorhandler/LoggingCSSParseErrorHandler.java
@@ -0,0 +1,139 @@
+/**
+ * Copyright (C) 2006-2014 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.css.reader.errorhandler;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.phloc.commons.annotations.Nonempty;
+import com.phloc.commons.string.ToStringGenerator;
+import com.phloc.css.parser.ParseException;
+import com.phloc.css.parser.Token;
+
+/**
+ * A logging implementation of {@link ICSSParseErrorHandler}
+ * 
+ * @author Philip Helger
+ */
+public class LoggingCSSParseErrorHandler implements ICSSParseErrorHandler
+{
+  private static final Logger s_aLogger = LoggerFactory.getLogger (LoggingCSSParseErrorHandler.class);
+  private static final int TOKEN_EOF = 0;
+
+  private final ICSSParseErrorHandler m_aNestedErrorHandler;
+
+  public LoggingCSSParseErrorHandler ()
+  {
+    this (null);
+  }
+
+  public LoggingCSSParseErrorHandler (@Nullable final ICSSParseErrorHandler aNestedErrorHandler)
+  {
+    m_aNestedErrorHandler = aNestedErrorHandler;
+  }
+
+  @Nonnull
+  @Nonempty
+  public static String createLoggingString (@Nonnull final Token aLastValidToken,
+                                            @Nonnull final int [][] aExpectedTokenSequencesVal,
+                                            @Nonnull final String [] aTokenImageVal,
+                                            @Nonnull final Token aLastSkippedToken)
+  {
+    if (aLastValidToken == null)
+      throw new NullPointerException ("LastValidToken");
+    if (aExpectedTokenSequencesVal == null)
+      throw new NullPointerException ("ExpectedTokenSequencesVal");
+    if (aTokenImageVal == null)
+      throw new NullPointerException ("TokenImageVal");
+
+    final StringBuilder aExpected = new StringBuilder ();
+    int nMaxSize = 0;
+    for (final int [] aExpectedTokens : aExpectedTokenSequencesVal)
+    {
+      if (nMaxSize < aExpectedTokens.length)
+        nMaxSize = aExpectedTokens.length;
+
+      if (aExpected.length () > 0)
+        aExpected.append (",");
+      for (final int nExpectedToken : aExpectedTokens)
+        aExpected.append (' ').append (aTokenImageVal[nExpectedToken]);
+    }
+
+    final StringBuilder retval = new StringBuilder ("[").append (aLastValidToken.next.beginLine)
+                                                        .append (':')
+                                                        .append (aLastValidToken.next.beginColumn)
+                                                        .append ("]-[")
+                                                        .append (aLastSkippedToken.endLine)
+                                                        .append (':')
+                                                        .append (aLastSkippedToken.endColumn)
+                                                        .append ("] Encountered");
+    Token aCurToken = aLastValidToken.next;
+    for (int i = 0; i < nMaxSize; i++)
+    {
+      retval.append (' ');
+      if (aCurToken.kind == TOKEN_EOF)
+      {
+        retval.append (aTokenImageVal[TOKEN_EOF]);
+        break;
+      }
+      retval.append ("token ")
+            .append (aTokenImageVal[aCurToken.kind])
+            .append (" with image '")
+            .append (aCurToken.image)
+            .append ('\'');
+      aCurToken = aCurToken.next;
+    }
+    retval.append (". Skipped until token ")
+          .append (aLastSkippedToken)
+          .append (". ")
+          .append (aExpectedTokenSequencesVal.length == 1 ? "Was expecting:" : "Was expecting one of:")
+          .append (aExpected);
+    return retval.toString ();
+  }
+
+  public void onCSSParseError (@Nonnull final Token aLastValidToken,
+                               @Nonnull final int [][] aExpectedTokenSequencesVal,
+                               @Nonnull final String [] aTokenImageVal,
+                               @Nonnull final Token aLastSkippedToken) throws ParseException
+  {
+    s_aLogger.warn (createLoggingString (aLastValidToken, aExpectedTokenSequencesVal, aTokenImageVal, aLastSkippedToken));
+
+    if (m_aNestedErrorHandler != null)
+      m_aNestedErrorHandler.onCSSParseError (aLastValidToken,
+                                             aExpectedTokenSequencesVal,
+                                             aTokenImageVal,
+                                             aLastSkippedToken);
+  }
+
+  public void onCSSUnexpectedRule (@Nonnull @Nonempty final String sRule, @Nonnull @Nonempty final String sMsg) throws ParseException
+  {
+    s_aLogger.warn ("Unexpected rule '" + sRule + "': " + sMsg);
+
+    if (m_aNestedErrorHandler != null)
+      m_aNestedErrorHandler.onCSSUnexpectedRule (sRule, sMsg);
+  }
+
+  @Override
+  public String toString ()
+  {
+    return new ToStringGenerator (this).toString ();
+  }
+}
